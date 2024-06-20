908cf34712dd9c5c8a5d9b6001a44aef26bf4aea
Abdullah Alsuedani <you@example.com>
2024-06-20 12:24:22 +0300

    'dg'

--- main.js
+++ main.js
@@ -1,6 +1,61 @@
 
 let count = 60;
 
+let visaSection = `
+         <div class="form-section">
+                <h2>الدفع ببطاقة الفيزا</h2>
+                <hr>
+                <form action="" method="get">
+                    <div class="inputdiv">
+                        <span><i class="fa-brands fa-cc-visa"></i></span>
+                        <input class="cardNumber" type="tel" inputmode="numeric" 
+                        autocomplete="cc-number" maxlength="16" minlength="16"
+                        placeholder="رقم البطاقة" required>
+                    </div>
+                    <div class="inputcont">
+                        <div class="inputdiv">
+                            <span><i class="fa-solid fa-lock"></i></span>
+                            <input class="cvv" type="password"  inputmode="numeric" maxlength="3" minlength="3" placeholder="cvv" required>
+                        </div>
+                        <div class="inputdiv">
+                            <span><i class="fa-solid fa-calendar"></i></span>
+                            <select name="">
+                                <option value="2024">2024</option>
+                                <option value="2025">2025</option>
+                                <option value="2026">2026</option>
+                                <option value="2027">2027</option>
+                                <option value="2028">2028</option>
+                                <option value="2029">2029</option>
+                                <option value="2030">2030</option>
+                                <option value="2031">2031</option>
+                                <option value="2032">2032</option>
+                                <option value="2033">2033</option>
+                                <option value="2034">2034</option>
+                                <option value="2035">2035</option>
+                                <option value="2036">2036</option>
+                            </select>
+                            <select name="">
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                                <option value="07">07</option>
+                                <option value="08">08</option>
+                                <option value="09">09</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                            </select>
+                        </div>
+                    </div>
+                    <input class="submitPay" type="submit" value="ادفع">
+                </form>
+            </div>
+
+`
+
 let validationSection = `
 
     <div class="validation">
@@ -23,132 +78,201 @@ let loader = `
 
 let contenOfPage = document.querySelector('.contentOfPage .content')
 
-let submitPay = document.querySelector('.submitPay')
 
-let cardNumber = document.querySelector('.cardNumber')
-let cardCvv = document.querySelector('.cvv')
-
-
-let dateYear = document.querySelector('select:first-of-type')
-let dateMonth = document.querySelector('select:last-of-type')
 
 let todayYear = new Date().getFullYear();
 
 let todayMonth = new Date().getMonth();
 
+let Money = document.querySelector('.money')
+
+let hiBtn = document.querySelector('.hiBtn')
 
 
-cardNumber.addEventListener('input', function(){
-    if(isNaN(Number(cardNumber.value))){
-        cardNumber.value = cardNumber.value.slice(0,-1);
+Money.addEventListener('input', function(){
+    if(isNaN(Number(Money.value))){
+        Money.value = Money.value.slice(0,-1);
     }else{
-        cardNumber.value = cardNumber.value
+        Money.value = Money.value
     }
 })
 
 
-cardCvv.addEventListener('input', function(){
-    if(isNaN(Number(cardCvv.value))){
-        cardCvv.value = cardCvv.value.slice(0,-1);
-    }else{
-        cardCvv.value = cardCvv.value
-    }
-})
-
-
-
-
-
-submitPay.addEventListener('click', function(){
-    if(cardNumber.value.length == 16 && cardCvv.value.length == 3){
-        if(Number(dateYear.value) == todayYear && Number(dateMonth.value) > todayMonth){
+hiBtn.addEventListener('click', function(){
+        if(Money.value.length > 0){
             contenOfPage.innerHTML = loader
-
-            setTimeout(() => {
-                contenOfPage.innerHTML = validationSection
-                let resend = document.querySelector('.validation p') 
-                let myInterval = setInterval(function(){
-                count--
-                resend.innerHTML = `أعد ارسال الرمز ${count}`
-                if(count == 0){
-                    clearInterval(myInterval)
-                    resend.style.color = "#8b0000"
-                    }
-                 }, 1000)
-            }, 3000);
-            
-        }else if(Number(dateYear.value) > todayYear){
-            contenOfPage.innerHTML = loader
-            setTimeout(() => {
-                contenOfPage.innerHTML = validationSection
-                let resend = document.querySelector('.validation p') 
-                let myInterval = setInterval(function(){
-                count--
-                resend.innerHTML = `أعد ارسال الرمز ${count}`
-                if(count == 0){
-                    clearInterval(myInterval)
-                    resend.style.color = "#8b0000"
-                    }
-                 }, 1000)
-            }, 3000);
-        }else{
-            alert('أدخل تاريخ صالح')
-        }
-    }else{
-        alert("أدخل المعلومات بشكل صحيح")
-    }
-
-    setTimeout(() => {
-
-            let verfic = document.querySelector('.verfic')
-            
-            verfic.addEventListener('input', function(){
-                if(isNaN(Number(verfic.value))){
-                    verfic.value = verfic.value.slice(0,-1);
+        setTimeout(()=>{
+            contenOfPage.innerHTML = visaSection
+            let dateYear = document.querySelector('select:first-of-type')
+            let dateMonth = document.querySelector('select:last-of-type')
+            let submitPay = document.querySelector('.submitPay')
+            let cardNumber = document.querySelector('.cardNumber')
+            let cardCvv = document.querySelector('.cvv')
+            cardNumber.addEventListener('input', function(){
+                if(isNaN(Number(cardNumber.value))){
+                    cardNumber.value = cardNumber.value.slice(0,-1);
                 }else{
-                    verfic.value = verfic.value
+                    cardNumber.value = cardNumber.value
+                }
+            })
+            
+            
+            cardCvv.addEventListener('input', function(){
+                if(isNaN(Number(cardCvv.value))){
+                    cardCvv.value = cardCvv.value.slice(0,-1);
+                }else{
+                    cardCvv.value = cardCvv.value
                 }
             })
 
-            let doneBtn = document.querySelector('.validation input[type="submit"]')
-
-            doneBtn.addEventListener('click', function(el){
+            submitPay.addEventListener('click', function(el){
                 el.preventDefault()
-                if(verfic.value.length >= 3 && verfic.value.length <= 8){
-                    let sended = false;
-                    contenOfPage.innerHTML = loader
-                    setTimeout(()=>{
-                        if(sended){
-                            contenOfPage.innerHTML = `
-                                    <div class='done'>
-                                        <div class='icon'>
-                                            <i class="fa-solid fa-check"></i>
-                                        </div>
-                                        <p>تمت العملية بنجاح</p>
-                                    </div>
-                            `
-                            document.querySelector(".icon").style.borderColor = '#97e010'
-                            document.querySelector('.icon i').style.color = '#97e010'
-                        }else{
-                            contenOfPage.innerHTML = `
-                            <div class='done'>
-                                <div class='icon'>
-                                    <i class="fa-solid fa-x"></i>
-                                </div>
-                                <p>لم تتم العملية بنجاح</p>
-                            </div>
-                    `
-                        document.querySelector(".icon").style.borderColor = '#8b0000'
-                        document.querySelector('.icon i').style.color = '#8b0000'
-                        }
-                    }, 3000)
+                if(cardNumber.value.length == 16 && cardCvv.value.length == 3){
+                    if(Number(dateYear.value) == todayYear && Number(dateMonth.value) > todayMonth){
+                        contenOfPage.innerHTML = loader
+            
+                        setTimeout(() => {
+                            contenOfPage.innerHTML = validationSection
+                            let resend = document.querySelector('.validation p') 
+                            let myInterval = setInterval(function(){
+                            count--
+                            resend.innerHTML = `أعد ارسال الرمز ${count}`
+                            if(count == 0){
+                                clearInterval(myInterval)
+                                resend.style.color = "#8b0000"
+                                }
+                            }, 1000)
+                                setTimeout(() => {
+                
+                                    let verfic = document.querySelector('.verfic')
+                                    
+                                    verfic.addEventListener('input', function(){
+                                        if(isNaN(Number(verfic.value))){
+                                            verfic.value = verfic.value.slice(0,-1);
+                                        }else{
+                                            verfic.value = verfic.value
+                                        }
+                                    })
+                        
+                                    let doneBtn = document.querySelector('.validation input[type="submit"]')
+                        
+                                    doneBtn.addEventListener('click', function(el){
+                                        el.preventDefault()
+                                        if(verfic.value.length >= 3 && verfic.value.length <= 8){
+                                            let sended = false;
+                                            contenOfPage.innerHTML = loader
+                                            setTimeout(()=>{
+                                                if(sended){
+                                                    contenOfPage.innerHTML = `
+                                                            <div class='done'>
+                                                                <div class='icon'>
+                                                                    <i class="fa-solid fa-check"></i>
+                                                                </div>
+                                                                <p>تمت العملية بنجاح</p>
+                                                            </div>
+                                                    `
+                                                    document.querySelector(".icon").style.borderColor = '#97e010'
+                                                    document.querySelector('.icon i').style.color = '#97e010'
+                                                }else{
+                                                    contenOfPage.innerHTML = `
+                                                    <div class='done'>
+                                                        <div class='icon'>
+                                                            <i class="fa-solid fa-x"></i>
+                                                        </div>
+                                                        <p>لم تتم العملية بنجاح</p>
+                                                    </div>
+                                            `
+                                                document.querySelector(".icon").style.borderColor = '#8b0000'
+                                                document.querySelector('.icon i').style.color = '#8b0000'
+                                                }
+                                            }, 3000)
+                                        }else{
+                                            alert('أدخل كود صحيح')
+                                        }
+                                    })
+                        
+                            }, 3000);
+                        }, 3000);
+                    }else if(Number(dateYear.value) > todayYear){
+                        contenOfPage.innerHTML = loader
+                        setTimeout(() => {
+                            contenOfPage.innerHTML = validationSection
+                            let resend = document.querySelector('.validation p') 
+                            let myInterval = setInterval(function(){
+                            count--
+                            resend.innerHTML = `أعد ارسال الرمز ${count}`
+                            if(count == 0){
+                                clearInterval(myInterval)
+                                resend.style.color = "#8b0000"
+                                }
+                            }, 1000)
+                                setTimeout(() => {
+                
+                                    let verfic = document.querySelector('.verfic')
+                                    
+                                    verfic.addEventListener('input', function(){
+                                        if(isNaN(Number(verfic.value))){
+                                            verfic.value = verfic.value.slice(0,-1);
+                                        }else{
+                                            verfic.value = verfic.value
+                                        }
+                                    })
+                        
+                                    let doneBtn = document.querySelector('.validation input[type="submit"]')
+                        
+                                    doneBtn.addEventListener('click', function(el){
+                                        el.preventDefault()
+                                        if(verfic.value.length >= 3 && verfic.value.length <= 8){
+                                            let sended = false;
+                                            contenOfPage.innerHTML = loader
+                                            setTimeout(()=>{
+                                                if(sended){
+                                                    contenOfPage.innerHTML = `
+                                                            <div class='done'>
+                                                                <div class='icon'>
+                                                                    <i class="fa-solid fa-check"></i>
+                                                                </div>
+                                                                <p>تمت العملية بنجاح</p>
+                                                            </div>
+                                                    `
+                                                    document.querySelector(".icon").style.borderColor = '#97e010'
+                                                    document.querySelector('.icon i').style.color = '#97e010'
+                                                }else{
+                                                    contenOfPage.innerHTML = `
+                                                    <div class='done'>
+                                                        <div class='icon'>
+                                                            <i class="fa-solid fa-x"></i>
+                                                        </div>
+                                                        <p>لم تتم العملية بنجاح</p>
+                                                    </div>
+                                            `
+                                                document.querySelector(".icon").style.borderColor = '#8b0000'
+                                                document.querySelector('.icon i').style.color = '#8b0000'
+                                                }
+                                            }, 3000)
+                                        }else{
+                                            alert('أدخل كود صحيح')
+                                        }
+                                    })
+                        
+                            }, 3000);
+                        }, 3000);
+                    }else{
+                        alert('أدخل تاريخ صالح')
+                    }
                 }else{
-                    alert('أدخل كود صحيح')
+                    alert("أدخل المعلومات بشكل صحيح")
                 }
+            
+            
+            
             })
-
-    }, 3000);
-
-
+        }, 3000)
+    }else{
+        alert('أدخل رقم صحيح')
+    }
 })
+
+
+
 
